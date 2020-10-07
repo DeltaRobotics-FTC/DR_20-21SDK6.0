38f6fd4f2b2a9f637ae7d8ca288524c4b4d66470
LauraAEvans <elzagirl10@gmail.com>
2020-10-06 19:49:57 -0500

    -made motorTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorTest.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "motorTest", group = "")
+public class motorTest extends LinearOpMode
+{
+    public void runOpMode()
+    {
+
+        double motorSpeed = 0;
+        boolean upstate = true;
+        boolean downstate = false;
+        boolean Astate = true;
+        boolean state = false;
+        RobotHardware robot = new RobotHardware(hardwareMap);
+        waitForStart();
+
+        while(opModeIsActive())
+        {
+
+            robot.motorRF.setPower(motorSpeed);
+            if(gamepad1.dpad_up)
+            {
+            upstate = false;
+            }
+            else
+            {
+            upstate = true;
+            }
+            if(!upstate && !downstate)
+            {
+            motorSpeed += 0.125;
+            downstate = true;
+            }
+            else
+            {
+            upstate = false;
+            }
+            telemetry.addData("motorspeed", motorSpeed);
+            telemetry.update();
+
+        }
+    }
+}
+
