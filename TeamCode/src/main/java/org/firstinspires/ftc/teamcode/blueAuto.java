df56d6d7e4795f60a5bf5f191cccd50f1438c75e
LauraAEvans <elzagirl10@gmail.com>
2020-10-24 12:27:50 -0500

    -updated blueAuto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blueAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blueAuto.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
-
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 //other imports
 
 //genneral
@@ -13,66 +14,153 @@ package org.firstinspires.ftc.teamcode;
 //auto aim
 
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
+import java.util.List;
+
 
 @TeleOp(name = "blueAuto")
-public class blueAuto extends LinearOpMode
-{
+public class blueAuto extends LinearOpMode {
 
+  private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
+  private static final String LABEL_FIRST_ELEMENT = "Quad";
+  private static final String LABEL_SECOND_ELEMENT = "Single";
   //init variables
-  
+  private static final String VUFORIA_KEY =
+          "AQIjJXP/////AAABmX8DXrmUxEBjvVNbT94EWcg3A75NZTjC3HG9/ur6NlOGrwrPUBWwLK8GlSeDl/fPcBsf+HkwYZQt7Fu8g/fJSvgftOYprWUaAWTCcyEnjfqU7CKCEEeWOO97PEJHdsjSPaRCoKAUjmRCknWJWxPuvgBXU4z63zwtr45AR0DzsF9FRdoj9pNR7hcmPKZmMLSfU6zdeBinzk2DQrJq2GGHJJgI0Mgh/IcrRA54NaGttRaqLpvLOuDHRiPyHnOtOXkjHBZp4Simdyqht675alc36Kyz3PF34/9X6m3b/43kuI231AaSBt1r5GnQv0jL9QRbGde2lr0U8mTmnatRm1ASpgCIcAJJ82jRpyWf3yELRH1w";
+  private VuforiaLocalizer vuforia;
+  private TFObjectDetector tfod;
   //genneral
-  
+
   //odometry
-  
+
   //TFOD
-  
+
   //velocity
-  
+
   //auto aim
 
+
   @Override
-    public void runOpMode() throws InterruptedException
-    {
+  public void runOpMode() throws InterruptedException {
+    initVuforia();
+    initTfod();
 
     //init 
-  
-    //genneral
-  
-    //odometry
-  
-    //TFOD
-  
-    //velocity
-  
-    //auto aim
-    
-    waitForStart();
-    
-    //Run Op Mode
-    
-    //see rings with tensor flow
+    if (tfod != null) {
+      tfod.activate();
 
-    //drive up to line and shoot with auto aim
+      // The TensorFlow software will scale the input images from the camera to a lower resolution.
+      // This can result in lower detection accuracy at longer distances (> 55cm or 22").
+      // If your target is at distance greater than 50 cm (20") you can adjust the magnification value
+      // to artificially zoom in to the center of image.  For best results, the "aspectRatio" argument
+      // should be set to the value of the images used to create the TensorFlow Object Detection model
+      // (typically 1.78 or 16/9).
 
-    //go place wabble goal
-
-    //drive back and pick up starter stack (if 4 shoot first 1-2 now with auto aim)
-
-    //pick up other wable goal
-
-    //grab any remaining rings 
-
-    //drive up to line
-
-    //shoot remaining rings with auto aim
-
-    //drop off wable goal
-
-    //drive to line
+      // Uncomment the following line if you want to adjust the magnification and/or the aspect ratio of the input images.
+      tfod.setZoom(3, 1.78);
     }
-  
+    //genneral
+
+    //odometry
+
+    //TFOD
+
+    //velocity
+
+    //auto aim
+    telemetry.addData(">", "Press Play to start op mode");
+    telemetry.update();
+    waitForStart();
+
+    while (opModeIsActive()) {
+      if (opModeIsActive()) {
+        while (opModeIsActive()) {
+          if (tfod != null) {
+            // getUpdatedRecognitions() will return null if no new information is available since
+            // the last time that call was made.
+            List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+            if (updatedRecognitions != null) {
+              telemetry.addData("# Object Detected", updatedRecognitions.size());
+              // step through the list of recognitions and display boundary info.
+              int i = 0;
+              for (Recognition recognition : updatedRecognitions) {
+                telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                        recognition.getLeft(), recognition.getTop());
+                telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                        recognition.getRight(), recognition.getBottom());
+              }
+              telemetry.update();
+            }
+          }
+        }
+      }
+
+      if (tfod != null) {
+        tfod.shutdown();
+      }
+      //Run Op Mode
+
+      //see rings with tensor flow
+
+      //drive up to line and shoot with auto aim
+
+      //go place wabble goal
+
+      //drive back and pick up starter stack (if 4 shoot first 1-2 now with auto aim)
+
+      //pick up other wable goal
+
+      //grab any remaining rings
+
+      //drive up to line
+
+      //shoot remaining rings with auto aim
+
+      //drop off wable goal
+
+      //drive to line
+    }
+  }
+
+
+
+  private void initVuforia() {
+    /*
+     * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+     */
+    VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+    parameters.vuforiaLicenseKey = VUFORIA_KEY;
+    parameters.cameraName = hardwareMap.get(WebcamName.class, "webcam");
+
+    //  Instantiate the Vuforia engine
+    vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+    // Loading trackables is not necessary for the TensorFlow Object Detection engine.
+  }
+
+  public void initTfod() {
+    int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+            "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+    TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+    tfodParameters.minResultConfidence = 0.6f;
+    tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+    tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
+  }
+
+}
+
+
+
+
   /*
   public voids
   */
@@ -85,4 +173,4 @@ public class blueAuto extends LinearOpMode
   
   //auto aim
   
-  }
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tensorFlowThread.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tensorFlowThread.java
@@ -1,36 +1,5 @@
-/* Copyright (c) 2019 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
@@ -52,11 +21,19 @@ import java.util.List;
  * is explained below.
  */
 @TeleOp(name = "Concept: TensorFlow Object Detection Webcam", group = "Concept")
-//@Disabled
-public class ConceptTensorFlowObjectDetectionWebcam extends LinearOpMode {
+@Disabled
+public class tensorFlowThread implements Runnable {
     private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
     private static final String LABEL_FIRST_ELEMENT = "Quad";
     private static final String LABEL_SECOND_ELEMENT = "Single";
+    private final double LABEL_FIRST_ELEMENT_NUMBER = 4;
+    private final double LABEL_SECOND_ELEMENT_NUMBER = 1;
+    private int sleepTime;
+    public String model(){ return TFOD_MODEL_ASSET; }
+    public String Quad(){ return LABEL_FIRST_ELEMENT; }
+    public String Single(){ return LABEL_SECOND_ELEMENT; }
+
+
 
     /*
      * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
@@ -85,8 +62,8 @@ public class ConceptTensorFlowObjectDetectionWebcam extends LinearOpMode {
      */
     private TFObjectDetector tfod;
 
-    @Override
-    public void runOpMode() {
+
+    public double runOpMode() {
         // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
         // first.
         initVuforia();
@@ -110,38 +87,34 @@ public class ConceptTensorFlowObjectDetectionWebcam extends LinearOpMode {
             tfod.setZoom(3, 1.78);
         }
 
-        /** Wait for the game to begin */
-        telemetry.addData(">", "Press Play to start op mode");
-        telemetry.update();
-        waitForStart();
 
-        if (opModeIsActive()) {
-            while (opModeIsActive()) {
+
+
                 if (tfod != null) {
                     // getUpdatedRecognitions() will return null if no new information is available since
                     // the last time that call was made.
                     List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
                     if (updatedRecognitions != null) {
-                      telemetry.addData("# Object Detected", updatedRecognitions.size());
-                      // step through the list of recognitions and display boundary info.
-                      int i = 0;
-                      for (Recognition recognition : updatedRecognitions) {
-                        telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
-                        telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
-                                recognition.getLeft(), recognition.getTop());
-                        telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
-                                recognition.getRight(), recognition.getBottom());
-                      }
-                      telemetry.update();
+                        //telemetry.addData("# Object Detected", updatedRecognitions.size());
+                        // step through the list of recognitions and display boundary info.
+                        int i = 0;
+                        for (Recognition recognition : updatedRecognitions) {
+                           // telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                           // telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                                    recognition.getLeft(), recognition.getTop());
+                            //telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                                    recognition.getRight(), recognition.getBottom());
+                        }
+
                     }
                 }
-            }
-        }
 
-        if (tfod != null) {
-            tfod.shutdown();
+
+        if (tfod != null){
+        tfod.shutdown();
         }
     }
+
 
     /**
      * Initialize the Vuforia localization engine.
@@ -166,10 +139,12 @@ public class ConceptTensorFlowObjectDetectionWebcam extends LinearOpMode {
      */
     public void initTfod() {
         int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
-            "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
-       tfodParameters.minResultConfidence = 0.6f;
-       tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
-       tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
+        tfodParameters.minResultConfidence = 0.6f;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
+    }
+    public void run() {
     }
 }
