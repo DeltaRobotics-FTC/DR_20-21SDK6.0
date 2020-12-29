676502d07205258574378adbb837197a31b9a8aa
DeltaRobotics-FTC <deltaroboticsftc@gmail.com>
2020-12-28 22:25:36 -0600

    added fast/slow modes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/workingMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/workingMecanum.java
@@ -13,7 +13,9 @@ public class workingMecanum extends LinearOpMode
     public void runOpMode() throws InterruptedException
     {
         double speed = 1.0;
-        double zScale = 1.0;
+        double zScale = 0.8;
+        double turnSpeed = 1.0;
+        double driveSpeed =1.0;
         RobotHardware robot = new RobotHardware(hardwareMap);
 
         waitForStart();
@@ -22,15 +24,31 @@ public class workingMecanum extends LinearOpMode
         {
 
             //sets the power of the motors
-            double LFpower = ( -gamepad1.right_stick_y + gamepad1.right_stick_x + (gamepad1.left_stick_x * zScale));
-            double LBpower = ( -gamepad1.right_stick_y - gamepad1.right_stick_x + (gamepad1.left_stick_x * zScale));
-            double RFpower = ( -gamepad1.right_stick_y - gamepad1.right_stick_x - (gamepad1.left_stick_x * zScale));
-            double RBpower = ( -gamepad1.right_stick_y + gamepad1.right_stick_x - (gamepad1.left_stick_x * zScale));
+            double LFpower = ( ((-gamepad1.right_stick_y + gamepad1.right_stick_x) * driveSpeed) + (gamepad1.left_stick_x * zScale * turnSpeed));
+            double LBpower = ( ((-gamepad1.right_stick_y - gamepad1.right_stick_x) * driveSpeed) + (gamepad1.left_stick_x * zScale * turnSpeed));
+            double RFpower = ( ((-gamepad1.right_stick_y - gamepad1.right_stick_x) * driveSpeed) - (gamepad1.left_stick_x * zScale * turnSpeed));
+            double RBpower = ( ((-gamepad1.right_stick_y + gamepad1.right_stick_x) * driveSpeed) - (gamepad1.left_stick_x * zScale * turnSpeed));
 
             robot.motorRF.setPower(RFpower * speed);
             robot.motorRB.setPower(RBpower * speed);
             robot.motorLB.setPower(LBpower * speed);
             robot.motorLF.setPower(LFpower * speed);
+            
+            if (gamepad1LeftJoysticButton == true) {
+                turnSpeed = 1.0;
+            }
+            
+            else {
+                turnSpeed = .5;
+            }
+            
+             if (gamepad1rightJoysticButton == true) {
+                driveSpeed = 1.0;
+            }
+            
+            else {
+                driveSpeed = .5;
+            }
         }
     }
 }
