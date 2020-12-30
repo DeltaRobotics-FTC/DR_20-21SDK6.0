d73b5d20aaff4bd23af43318f7074d98336faf8e
DeltaRobotics-FTC <deltaroboticsftc@gmail.com>
2020-12-29 22:20:53 -0600

    added servos, intake motors, and commented

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -12,17 +12,26 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class RobotHardware
 {
+    //drive motors
     public DcMotor motorRF = null;
     public DcMotor motorLF = null;
     public DcMotor motorRB = null;
     public DcMotor motorLB = null;
+    
+    //mechanism motors
     public DcMotor flywheel = null;
     public DcMotor wobble = null;
-    //public DcMotor intake1 = null;
+    public DcMotor intake1 = null;
+    public DcMotor intake2 = null;
 
+    //odometry encoders
     public DcMotor verticalRight = null;
     public DcMotor verticalLeft = null;
     public DcMotor horizontal = null;
+    
+    //servos
+    public Servo servo = null;
+    public Servo servo2 = null;
 
     boolean useEncoder = false;
 
@@ -31,34 +40,38 @@ public class RobotHardware
 
     public RobotHardware(HardwareMap ahwMap)
     {
+        //dive motors
         motorRF = ahwMap.dcMotor.get("motorRF");
         motorLF = ahwMap.dcMotor.get("motorLF");
         motorRB = ahwMap.dcMotor.get("motorRB");
         motorLB = ahwMap.dcMotor.get("motorLB");
+        
+        //mechanism motors
         flywheel = ahwMap.dcMotor.get("flywheel");
         wobble = ahwMap.dcMotor.get("wobble");
-        //intake1 = ahwMap.dcMotor.get("intake1");
+        intake1 = ahwMap.dcMotor.get("intake1");
+        intake2 = ahwMap.dcMotor.get("intake2");
 
-
-
+        //odometry encoders
         verticalRight = motorLF;
         verticalLeft = motorRF;
         horizontal = motorRB;
 
+        //servos
+        servo = hardwareMap.servo.get("servo");
+        servo2 = hardwareMap.servo.get("servo2");
+        
+        //drive motors and odometry encoders
         motorRF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorLF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorRB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorLB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        flywheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        wobble.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
+        
         motorRF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorLF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorRB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorLB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        wobble.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
+        
         motorLF.setDirection(DcMotorSimple.Direction.REVERSE);
         motorLB.setDirection(DcMotorSimple.Direction.REVERSE);
 
@@ -66,10 +79,26 @@ public class RobotHardware
         motorLF.setPower(0);
         motorRB.setPower(0);
         motorLB.setPower(0);
+        
+        //mechanism motors
+        flywheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        wobble.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intake1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        intake2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        wobble.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        intake1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        intake2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
         flywheel.setPower(0);
         wobble.setPower(0);
-
-        //intake1.setPower(0);
+        intake1.setPower(0);
+        intake1.setPower(0);
+        
+        //servos
+        servo.setPosition(0);
+        servo2.setPosition(0);
     }
 
 }
