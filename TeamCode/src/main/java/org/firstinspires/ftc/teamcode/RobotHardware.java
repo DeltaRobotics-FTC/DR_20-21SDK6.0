e1becad62400cae42db89a3e53d39c8862a8efb1
DeltaRobotics-FTC <deltaroboticsftc@gmail.com>
2020-12-29 22:01:27 -0600

    added flywheel and wobble to HWmap

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -16,6 +16,8 @@ public class RobotHardware
     public DcMotor motorLF = null;
     public DcMotor motorRB = null;
     public DcMotor motorLB = null;
+    public DcMotor flywheel = null;
+    public DcMotor wobble = null;
     //public DcMotor intake1 = null;
 
     public DcMotor verticalRight = null;
@@ -33,6 +35,8 @@ public class RobotHardware
         motorLF = ahwMap.dcMotor.get("motorLF");
         motorRB = ahwMap.dcMotor.get("motorRB");
         motorLB = ahwMap.dcMotor.get("motorLB");
+        flywheel = ahwMap.dcMotor.get("flywheel");
+        wobble = ahwMap.dcMotor.get("wobble");
         //intake1 = ahwMap.dcMotor.get("intake1");
 
 
@@ -45,11 +49,15 @@ public class RobotHardware
         motorLF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorRB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorLB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        flywheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        wobble.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         motorRF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorLF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorRB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorLB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        wobble.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         motorLF.setDirection(DcMotorSimple.Direction.REVERSE);
         motorLB.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -58,6 +66,8 @@ public class RobotHardware
         motorLF.setPower(0);
         motorRB.setPower(0);
         motorLB.setPower(0);
+        flywheel.setPower(0);
+        wobble.setPower(0);
 
         //intake1.setPower(0);
     }
