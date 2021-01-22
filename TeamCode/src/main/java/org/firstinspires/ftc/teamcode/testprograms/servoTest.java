13a1794e33db1b9bb9800fb311602dcace15621a
LauraAEvans <elzagirl10@gmail.com>
2021-01-21 19:31:15 -0600

    updated RR

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testprograms/servoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testprograms/servoTest.java
@@ -3,51 +3,102 @@ package org.firstinspires.ftc.teamcode.testprograms;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
+/**
+ * Created by User on 10/5/2017.
+ */
 
-@TeleOp(name = "ServoTest", group = "")
-@Disabled
+@TeleOp (name = "ServoTest", group = "")
 public class ServoTest extends LinearOpMode
 {
-    private Servo servo;
 
+    Servo servo;
+
+    boolean leftDPadState = false;
+    boolean rightDPadState = false;
+
+    boolean fastIncrement = true;
+
+    double servoPosition = 0.5;
 
     @Override
     public void runOpMode()
     {
         servo = hardwareMap.servo.get("servo");
-        double waitTime = 1000;
-        double servoPosition = 0;
 
-        boolean fastIncrement = true;
 
+        servo.setPosition(servoPosition);
 
         waitForStart();
 
-        while (opModeIsActive())
+        while(opModeIsActive())
         {
+            if(gamepad1.a)
+            {
+                fastIncrement = true;
+            }
+
+            if(gamepad1.b)
+            {
+                fastIncrement = false;
+            }
+
+            if(gamepad1.dpad_left && !leftDPadState)
+            {
+                leftDPadState = true;
+                if(fastIncrement)
+                {
+                    servoPosition -= 0.05;
+                }
+                else
+                {
+                    servoPosition -= 0.01;
+                }
+            }
+
+            else if(!gamepad1.dpad_left)
+            {
+                leftDPadState = false;
+            }
+
+            if(gamepad1.dpad_right && !rightDPadState)
+            {
+                rightDPadState = true;
+
+                if(fastIncrement)
+                {
+                    servoPosition += 0.05;
+                }
+                else
+                {
+                    servoPosition += 0.01;
+                }
+            }
+
+
+            else if(!gamepad1.dpad_right)
+            {
+                rightDPadState = false;
+            }
+
+            if(servoPosition > 1.0)
+            {
+                servoPosition = 1.0;
+            }
+
+            if(servoPosition < 0.0)
+            {
+                servoPosition = 0.0;
+            }
+
             servo.setPosition(servoPosition);
-            waitTime++;
 
-            if (gamepad1.a)
-            {
-                servoPosition = servoPosition + 0.1;
-            }
-            if (gamepad1.b)
-            {
-                servoPosition = servoPosition - 0.1;
-            }
-
-            if (gamepad1.dpad_up)
-            {
-                servoPosition = 0;
-            }
             telemetry.addData("servo Pos", servo.getPosition());
             telemetry.addData("servoPosition Var", servoPosition);
             telemetry.addData("Fast Increment", fastIncrement);
             telemetry.update();
+
         }
+
     }
 }
